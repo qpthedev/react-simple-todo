8999bbe24d44e87bb4dbac63315d810e74a60dab
qpthedev <qpthedev@gmail.com>
2021-12-01 01:12:41 -0500

    category selectors

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,10 +1,21 @@
 import { atom, selector } from "recoil";
 
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
+
+export const categoryState = atom<Categories>({
+  key: "category",
+  default: Categories.TO_DO,
+});
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
@@ -15,11 +26,8 @@ export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
+    const category = get(categoryState);
 
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
